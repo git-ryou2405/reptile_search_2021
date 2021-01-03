120acf17df3a133613e7a4763c9a78e68a536506
git-ryou2405 <r2405.exe@gmail.com>
2021-01-03 14:02:21 +0900

    deviseルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
   
-  devise_for :shops
-  
+  devise_for :shops, controllers: {
+    registrations: 'shops/registrations',
+    sessions: 'shops/sessions',
+    passwords: 'users/passwords'
+  }
   resources :shops
 end
